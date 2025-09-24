057c2986f14e55b1b47563c1695c46869e9c1e9f
splanzer2 <splanzer@student.ethz.ch>
2025-09-24 08:13:19 +0200

    Add files via upload

---
+++ ts_2025_exercise1_template.R
@@ -0,0 +1,85 @@
+# ---------------------------
+#
+# Script name:     
+# Purpose of script: 
+# Author: 
+# Date Created: 
+#
+# ---------------------------
+
+# load all necessary packages
+
+
+
+# Exercise 1 --------------------------------------------------------------
+
+## 1.1	Exploring descriptive statistics ----------------------------------
+
+# load data (you can use the load() function since it is a RData file)
+
+
+
+## 1.2	Calculating statistics by group -------------------------------------
+
+
+
+
+## 1.3	Creating a bar chart (plotting a categorical and numerical variable) -------
+
+
+
+
+
+## 1.4	Creating a line chart (plotting two numerical variables) ------------
+
+
+
+
+# Exercise 1: MOBIS SOCIOECONOMIC DATA -------------------------------------
+
+
+## 2.1	Summary statistics of socioeconomic variables -----------------------
+
+# load dataset (you will need to read the csv file)
+
+# tidy dataset
+
+
+
+##  2.2	Comparing socioeconomic characteristics between the MOBIS sample and the microcensus --------
+
+## 2.3	Plotting the relationship between age and income ----------------------
+
+
+
+## 2.4	Checking for a normal distribution -----------------------------------
+
+
+
+
+
+
+# 3	MOBIS: TRAVEL BEHAVIOUR -----------------------------------------------
+
+
+## 3.1	Summary trip statistics ---------------------------------------------
+
+
+
+
+
+## 3.2	Calculating mode share ----------------------------------------------
+
+
+
+
+
+## 3.3	Mode share vs income and age ----------------------------------------
+
+
+
+
+## 3.4	Average daily passenger-kilometres travelled per person per per mode, by income and age group --------
+
+
+
